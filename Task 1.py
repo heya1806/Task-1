774e98aa646d9dfcce2b29277d1a5de6014e5094
heya1806 <heyachhajer@gmail.com>
2025-06-09 23:51:47 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,14 @@
+#Perform Basic Mathematical Operations
+x=input('enter the first number: ')
+y=input('enter the second number: ')
+x=float(x)
+y=float(y)
+a=x+y
+s=x-y
+m=x*y
+d=x/y
+print('Addition: ',a)
+print('Subtraction: ',s)
+print('Multiplication: ',m)
+print('Division: ',d)
+
---
+++ Task-2.py
@@ -0,0 +1,5 @@
+#Personalised greeting
+x=input('Enter your first name: ')
+y=input('Enter your last name: ')
+print('Hello,',x,y+'! Welcome to the Python program.')
+
