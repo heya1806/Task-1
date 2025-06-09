9bdbdc3a69e9206a21d0cf5c56ccdef300b2ef80
heya1806 <heyachhajer@gmail.com>
2025-06-09 23:59:02 +0530

    initial commit

---
+++ README.md.py
@@ -0,0 +1,12 @@
+#Basic Python Concepts
+This repository includes two python scripts demonstrating the use of input and print functions in Python
+#Task1
+Perform Basic Mathematical Operations
+
+#Task 2
+Create a personalised Greeting
+
+##How to run
+-Open the files in Pycharm
+_Run the scripts
+-follow The input prompts
