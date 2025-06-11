e51cda451812f4801be7143ade1050af1d375612
heya1806 <heyachhajer@gmail.com>
2025-06-11 17:51:35 +0530

    initial commit

---
+++ Task-2(Assignment-4).py
@@ -0,0 +1,12 @@
+# Using the math module for calculations
+
+n=int(input('Enter a number: '))
+
+from math import sqrt
+print('Square root:', sqrt(n))
+
+from math import log
+print('Logarithm:', log(n))
+
+from math import sin
+print('Sine:', sin(n))
